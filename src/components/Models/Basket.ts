9b3a8f6eedd132c131b0fe04b10584567c058724
Maria Glazkova <marusya86_msk@yandex.ru>
2025-09-29 00:30:49 +0300

    Добавила методы в Basket.ts

--- src/components/Models/Basket.ts
+++ src/components/Models/Basket.ts
@@ -12,26 +12,37 @@ export class Basket {
   }
 
   add(item: IProduct): void {
-
+    this.items.push(item);
   }
 
   remove(id: string): void {
+    this.items = this.items.filter((item) => item.id != id);
 
   }
 
   clear(): void {
-
+    this.items = [];
   }
 
   getTotal(): number {
-
+    let sum = 0;
+    for (const item of this.items) {
+      if (item.price)
+        sum += item.price;
+    }
+    return sum;
   }
 
   getCount(): number {
+    return this.items.length;
 
   }
 
   has(id: string): boolean {
-
+    for (const item of this.items) {
+      if (item.id === id)
+        return true;
+    }
+    return false;
   }
 };
--- src/main.ts
+++ src/main.ts
@@ -16,4 +16,22 @@ console.log('Выбранный товар из каталога: ', productsMod
 
 const basketModel = new Basket();
 
+console.log('Mассив товаров в пустой корзине: ', basketModel.getItems());
+console.log('Количество товаров в пустой корзине: ', basketModel.getCount());
+console.log('Общая стоимость товаров в пустой корзине: ', basketModel.getTotal());
+basketModel.add(apiProducts.items[0]);
+basketModel.add(apiProducts.items[2]);
+console.log('Mассив товаров в корзине: ', basketModel.getItems());
+console.log('Количество товаров в корзине: ', basketModel.getCount());
+console.log('Общая стоимость товаров в корзине: ', basketModel.getTotal());
+console.log('Наличие 0ого товара в корзине: ', basketModel.has(apiProducts.items[0].id));
+console.log('Наличие 1ого товара в корзине: ', basketModel.has(apiProducts.items[1].id));
+basketModel.remove(apiProducts.items[2].id);
+console.log('Mассив товаров в корзине после удаления: ', basketModel.getItems());
+console.log('Количество товаров в корзине после удаления: ', basketModel.getCount());
+basketModel.clear();
+console.log('Mассив товаров в корзине после очистки: ', basketModel.getItems());
+console.log('Количество товаров в корзине после очистки: ', basketModel.getCount());
+
+
 const buyerModel = new Buyer();
